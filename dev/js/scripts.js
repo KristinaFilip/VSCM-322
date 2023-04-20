8af22d47695b5e32cffffe9e6c6fd6dfaf0e024c
KristinaFilip <kristinafilip18@gmail.com>
2023-04-20 08:42:45 -0400

    GSAP

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,55 @@
 import { gsap } from "gsap";
+
+gsap.set(".red-box", {x:200});
+
+//Keywords - to store data
+
+//var - stands for variable, older, avoid
+//const new ES6 cannot be updated or re-declared
+//let - also new to ES6 imporved var "block-scoped{}"
+//const tweenDur = "0.5"; - stands for constant, does not change
+
+// variable syntax = keyword, referenceName assignmentOperator, value;
+
+//let tweenDur = "0.5";
+
+//General Rules for reference name;
+// 1. Names can contain letters, digits, underscores, and dollar signs.
+// 2. Names must begin with a letter or can also begin with $ and _
+// 3. Names are case sensitive (y and Y are different valuables)
+// 4. Reserved words (like JavaScript keywords) cannot be used as names
+
+//examples
+// let TweenDuration = "0.5";
+// let someElement = document.querySelector('.red-box');
+// let allElementsWithClass = document.querySelectorAll('.box');
+// let mainTl = gsap.timeline();
+
+//GSAP
+
+//gsap.to(target, {duration, vars, ease});
+//example: gsap.set(".target", {x:200});
+//example: gsap.to(."target", {duration:2, x:200});
+//example: gsap.from(."target", {duration:2, scale:2, roation:180});
+
+//.set - no duration, happens immediately
+//.to - moves object to where you want it to go
+//.from - starts from properties you input and then goes back to original
+
+//let someTween = gsap.to(target", {duration:0.25, x:200});
+
+//CSS
+
+//example: gsap.to(."target", {duration:2, backgroundColor:"black"});
+
+//Transforms = (rotation, scaleX, scaleY, skewX, skewY, x, y, rotationX, and rotationY, alpha, aytoalpha);
+//Special Properties = duration, callbacks, delays, easing, staggers, repeat, yoyo, repeat, delay
+
+
+gsap.to(".element", {duration:1, x:20});
+gsap.to(".button", {duration:1.5, x:20});
+gsap.from(".element", {scale:1.2});
+
+
+
+
