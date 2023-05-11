40603f2d6f1a0591221870124bf7dfa4f0eeda52
KristinaFilip <kristinafilip18@gmail.com>
2023-05-11 10:25:29 -0400

    scripts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -13,12 +13,11 @@ function section1Ani (){
     let tl = gsap.timeline({});
     tl.to(".images1", {duration:1.5, ease:"power4.out", x:20}, 0)
     .from(".images1", {duration:1.5, alpha:0.25}, 0)
-    .to(".h1", {duration:1.5, ease:"power4.out", x:20}, 0)
-    .from(".h1", {duration:1.5, alpha:0.25}, 0)
+    .to(".title1", {duration:1.5, ease:"power4.out", x:20}, 0)
+    .from(".title1", {duration:1.5, alpha:0.25}, 0)
 
     return tl;
 }
-
 
 mainTL.add(section1Ani(), 0)
 
