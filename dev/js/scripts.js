70c4c306e08fce5aa5844aa65730bf1969bdb848
KristinaFilip <kristinafilip18@gmail.com>
2023-05-11 10:03:26 -0400

    scripts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,3 +2,21 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+//TIMELINE
+
+let mainTL = gsap.timeline({});
+
+//FUNCTION 
+
+function section1Ani (){
+    let tl = gsap.timeline({});
+    tl.to(".images", {duration:1.5, ease:"power4.out", x:20})
+
+    return tl;
+}
+
+
+mainTL.add(section1Ani(), 0)
+
+;
