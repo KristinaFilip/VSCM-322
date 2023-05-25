0dbbcc6f3939b455bf0c568f01276b4789481ed3
KristinaFilip <kristinafilip18@gmail.com>
2023-05-25 12:36:16 -0400

    Add scripts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,18 @@
 //import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools); //MorphSVGPlugin
+
+const mainTl = gsap.timeline();
+
+mainTl.from("", {duration:1, drawSVG:"100%"})
+
+
+;
+
+
+
+GSDevTools.create();
