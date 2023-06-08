c03844ec8e4a24e9f7c337c4ff6f7fb08a972a77
KristinaFilip <kristinafilip18@gmail.com>
2023-06-08 06:56:41 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,13 +2,14 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+//import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools); //MorphSVGPlugin
 
 const mainTl = gsap.timeline();
 
-mainTl.from("", {duration:1, drawSVG:"100%"})
+mainTl.from("darkblue-circle1", {duration:1, scale:0})
+.to("darkblue-circle1", {duration:1, scale:100})
 
 
 ;
