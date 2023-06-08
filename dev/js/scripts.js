ef377745698cda9c0001df877dbd9f6704a638c7
KristinaFilip <kristinafilip18@gmail.com>
2023-06-08 06:35:07 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,42 +3,40 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools); MorphSVGPlugin
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin, GSDevTools);
 
+
+let mainTl = gsap.timeline({});
 
 function museumAnimation(){
-    let tl = gsap.timeline();
+    let tl = gsap.timeline({});
 
-    tl.from("#m1-draw", {duration:1, drawSVG:"100%"})
-    .from("#u1-draw", {duration:1, drawSVG:"100%"})
-    .from("#s-draw", {duration:1, drawSVG:"100%"})
-    .from("#u2-draw", {duration:1, drawSVG:"100%"})
-    .from("#e1-draw", {duration:1, drawSVG:"100%"})
-    .from("#e2-draw", {duration:1, drawSVG:"100%"})
-    .from("m2-draw", {duration:1, drawSVG:"100%"})
+    tl.from("#m1-draw", {duration:1, drawSVG:"100%"}, 0)
+    .from("#u1-draw", {duration:1, drawSVG:"100%"}, 0)
+    .from("#s-draw", {duration:1, drawSVG:"100%"}, 0)
+    .from("#u2-draw", {duration:1, drawSVG:"100%"}, 0)
+    .from("#e1-draw", {duration:1, drawSVG:"100%"}, 0)
+    .from("#e2-draw", {duration:1, drawSVG:"100%"}, 0)
+    .from("m2-draw", {duration:1, drawSVG:"100%"}, 0)
+
+    return tl;
 }
 
 function mutterAnimation(){
     let tl = gsap.timeline();
 
-    tl.from("m", {duration:1, y:100})
+    tl.from("m", {duration:1, alpha:0.25})
+
+    return tl;
 }
 
 
 
-let mainTl = gsap.timeline();
 mainTl.add(museumAnimation())
-.add(museumAnimation())
+.add(mutterAnimation())
 
 ;
 
 
 
 GSDevTools.create();
-
-
-
-
-//const mainTl = gsap.timeline({id:"mainTl"});
-//mainTl.from("#MÃ¼tter", {duration:1, drawSVG:"50%"})
-//mainTl.from("#MÃ¼tter", {duration:1, morphSVG:"50%"})
