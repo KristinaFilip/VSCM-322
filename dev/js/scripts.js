3ffabe54bc85172fb0365feab9308c5f34bd3891
KristinaFilip <kristinafilip18@gmail.com>
2023-06-08 06:24:56 -0400

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,15 +3,42 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools); //MorphSVGPlugin
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools); MorphSVGPlugin
 
-const mainTl = gsap.timeline();
 
-mainTl.from("#Pet", {duration:1, drawSVG:"100%"})
+function museumAnimation(){
+    let tl = gsap.timeline();
 
+    tl.from("#m1-draw", {duration:1, drawSVG:"100%"})
+    .from("#u1-draw", {duration:1, drawSVG:"100%"})
+    .from("#s-draw", {duration:1, drawSVG:"100%"})
+    .from("#u2-draw", {duration:1, drawSVG:"100%"})
+    .from("#e1-draw", {duration:1, drawSVG:"100%"})
+    .from("#e2-draw", {duration:1, drawSVG:"100%"})
+    .from("m2-draw", {duration:1, drawSVG:"100%"})
+}
+
+function mutterAnimation(){
+    let tl = gsap.timeline();
+
+    tl.from("m", {duration:1, y:100})
+}
+
+
+
+let mainTl = gsap.timeline();
+mainTl.add(museumAnimation())
+.add(museumAnimation())
 
 ;
 
 
 
 GSDevTools.create();
+
+
+
+
+//const mainTl = gsap.timeline({id:"mainTl"});
+//mainTl.from("#MÃ¼tter", {duration:1, drawSVG:"50%"})
+//mainTl.from("#MÃ¼tter", {duration:1, morphSVG:"50%"})
